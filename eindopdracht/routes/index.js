4237178fe903a67fa02c8b87d5030d5d8d44e3b1
louishol <louis_hol@hotmail.com>
2015-03-26 14:08:40 +0100

    Alle files op de server

--- eindopdracht/routes/index.js
+++ eindopdracht/routes/index.js
@@ -1,9 +1,80 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http'),
+    fs = require('fs');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/js/:jsfile', function(req, res, next) {
+
+	var file = req.params.jsfile;
+	fs.readFile('./website/js/'+file, function (err, js) {
+
+		if(err)
+		{
+			throw err;
+		}
+		else
+		{
+        	res.write(js);  
+        	res.end();
+		}
+
+	});
+});
+router.get('/css/:cssfile', function(req, res, next) {
+	var file = req.params.cssfile;
+	fs.readFile('./website/css/'+file, function (err, js) {
+		if(err)
+		{
+			throw err;
+		}
+		else
+		{
+        	res.write(js);  
+        	res.end();
+		}
+	});
+});
+
+router.get('/management', function(req, res) {
+	fs.readFile('./website/management.html', function (err, html) {
+		if(err)
+		{
+			throw err;
+		}
+		else
+		{
+			res.writeHeader(200, {"Content-Type": "text/html"});  
+        	res.write(html);  
+        	res.end();
+		}
+	});
+});
+
+
+router.get('/ìndex', function(req, res) {
+	fs.readFile('./website/index.html', function (err, html) {
+		if(err)
+		{
+			throw err;
+		}
+		else
+		{
+			res.writeHeader(200, {"Content-Type": "text/html"});  
+        	res.write(html);  
+        	res.end();
+		}
+	});
+
+});
+
+
+
 
 module.exports = router;
