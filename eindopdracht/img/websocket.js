54f936bf789c091aeb68f51de2db490bbfd8d170
louishol <louis_hol@hotmail.com>
2015-03-31 18:37:51 +0200

    Websocket fix

--- eindopdracht/img/websocket.js
+++ eindopdracht/img/websocket.js
@@ -1,37 +1,33 @@
-// if user is running mozilla then use it's built-in WebSocket
-window.WebSocket = window.WebSocket || window.MozWebSocket;
- if (!window.WebSocket) {
-       alert("Deze browser wordt niet ondersteunt");
- }
-var connection = new WebSocket("ws://"+window.location.hostname+":4000");
-console.log("In WebSocket");
+var connection
+window.addEventListener("load", function () {
 
-connection.onopen = function () {
- console.log("Iemand heeft m geopend");
-};
-connection.onerror = function (error) {
-	alert("Error : "+ error);
-};
+		connection = new WebSocket("ws://"+window.location.hostname+":8081")
+		connection.onopen = function () { }
 
-connection.onmessage = function (message) {
-	
-	var json = JSON.parse(message.data);
-	var race = json.data.race;
-	var user = json.data.user;
-	var waypoint = json.data.waypoint;
-	var winner = json.data.winner;
-	
-	var raceid = $("#races").val();
-	if(raceid == race)
-	{
-		$("#"+waypoint).append("<li>" + user + "</li>");
-		if(winner != null)
-		{
-			alert("Er heeft zojuist iemand de race gewonnen, van harte gefeliciteerd " + winner);
+		connection.onclose = function () {
+			console.log("Connection closed")
 		}
-	}
-};
-
+		connection.onerror = function () {
+			console.error("Connection error")
+		}
+		connection.onmessage = function (evt) {
+			var json = JSON.parse(evt.data);
+			var race = json.data.race;
+			var user = json.data.user;
+			var waypoint = json.data.waypoint;
+			var winner = json.data.winner;
+			var raceid = $("#races").val();
+			if(raceid == race)
+			{
+				$("#"+waypoint).append("<li>" + user + "</li>");
+				if(winner != null)
+				{
+					alert("Er heeft zojuist iemand de race gewonnen, van harte gefeliciteerd " + winner);
+				}
+			}
+		}
+		
+});
 function sendMessageToClients(message)
 {
 	 connection.send(message);
--- eindopdracht/websocket/websocket.js
+++ eindopdracht/websocket/websocket.js
@@ -1,43 +1,12 @@
-// Port where we'll run the websocket server
-var webSocketsServerPort = 4000;
- 
-// websocket and http servers
-var webSocketServer = require('websocket').server;
-var http = require('http');
-// list of currently connected clients (users)
-var clients = [ ];
-/**
- * HTTP server
- */
-var server = http.createServer(function(request, response) {
-    // Not important for us. We're writing WebSocket server, not HTTP server
-});
-server.listen(webSocketsServerPort, function() {
-    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
-});
-/**
- * WebSocket server
- */
-var wsServer = new webSocketServer({
-    // WebSocket server is tied to a HTTP server. WebSocket request is just
-    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
-    httpServer: server
-});
 
-wsServer.on('request', function(request) {
-console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-// accept connection - you should check 'request.origin' to make sure that
-// client is connecting from your website
-// (http://en.wikipedia.org/wiki/Same_origin_policy)
-var connection = request.accept(null, request.origin); 
-// we need to know client index to remove them on 'close' event
+var http = require("http");
+var ws = require("nodejs-websocket")
+var fs = require("fs");
 
-var index = clients.push(connection) - 1;
-console.log("Nieuwe connectie, aantal connecites zijn :" + clients.length);
-// user sent some message
-connection.on('message', function(message) {
-    console.log(message);
-    var _message  = JSON.parse(message.utf8Data);
+
+var server = ws.createServer(function (connection) {
+    connection.on("text", function (str) {
+    var _message  = JSON.parse(str);
     var obj = 
     {
         race: _message.race,
@@ -45,17 +14,18 @@ connection.on('message', function(message) {
         waypoint: _message.waypoint,
         winner: _message.winner
     };
-
     var json = JSON.stringify({ type:'message', data: obj });
-    for (var i=0; i < clients.length; i++) {
-        clients[i].sendUTF(json);
-    }
-});
-// user disconnected
-connection.on('close', function(connection) {
-        // remove user from the list of connected clients
-        clients.splice(index, 1);
-    
-});
+     broadcast(json)
+    })
+    connection.on("error", function(err)
+    {
 
-});
+    })
+})
+server.listen(8081)
+
+function broadcast(str) {
+    server.connections.forEach(function (connection) {
+        connection.sendText(str)
+    })
+}
