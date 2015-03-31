6e4db206e147bc1e45aa88f4fa4c6a914346a6dc
louishol <louis_hol@hotmail.com>
2015-03-31 21:14:34 +0200

    TROL FIX

--- dataAccess/database.js
+++ dataAccess/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 	db: {
-		dev: ' mongodb://admin:admin@ds041377.mongolab.com:41377/eindopdracht',
+		dev: 'mongodb://admin:admin@ds041377.mongolab.com:41377/eindopdracht',
 		test: 'mongodb://admin:admin@ds041377.mongolab.com:41377/eindopdracht'
 	}
 };  
