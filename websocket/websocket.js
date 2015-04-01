a986e1503850c77e0d9728371d36849967ec5d9b
louishol <louis_hol@hotmail.com>
2015-04-01 13:50:15 +0200

    Zonder poort

--- websocket/websocket.js
+++ websocket/websocket.js
@@ -1,37 +1,8 @@
-// var WebSocketServer = require("ws").Server
-// var http = require("http")
-// var express = require("express")
-// var app = express()
-// var port = process.env.PORT || 6000
-
-// app.use(express.static(__dirname + "/"))
-
-// var server = http.createServer(app)
-// server.listen(port)
-
-// console.log("http server listening on %d", port)
-
-// var wss = new WebSocketServer({server: server})
-// console.log("websocket server created")
-
-// wss.on("connection", function(ws) {
-//   var id = setInterval(function() {
-//     ws.send(JSON.stringify(new Date()), function() {  })
-//   }, 1000)
-
-//   console.log("websocket connection open")
-
-//   ws.on("close", function() {
-//     console.log("websocket connection close")
-//     clearInterval(id)
-//   })
-// })
-
 var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+var port =  5000
 
 app.use(express.static(__dirname + "/"))
 
@@ -44,9 +15,10 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
+  
   var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  }, 5000)
 
   console.log("websocket connection open")
 
