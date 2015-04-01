a2e64ad778b48d8ae694235dec11b5e5c62c0a30
louishol <louis_hol@hotmail.com>
2015-04-01 14:30:02 +0200

    websocket

--- websocket/websocket.js
+++ websocket/websocket.js
@@ -1,29 +1,35 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port =  5000
 
-app.use(express.static(__dirname + "/"))
+var http = require("http");
+var ws = require("nodejs-websocket")
+var fs = require("fs");
 
-var server = http.createServer(app)
-server.listen(port)
 
-console.log("http server listening on %d", port)
+var server = ws.createServer(function (connection) {
+    console.log("Er is een nieuwe verbinding");
+    connection.on("text", function (str) {
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
 
-wss.on("connection", function(ws) {
-  
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 5000)
+    console.log("Nieuwe message");
+    var _message  = JSON.parse(str);
+    var obj = 
+    {
+        race: _message.race,
+        user: _message.user,
+        waypoint: _message.waypoint,
+        winner: _message.winner
+    };
+    var json = JSON.stringify({ type:'message', data: obj });
+     broadcast(json)
+    })
+    connection.on("error", function(err)
+    {
 
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
+    })
 })
+server.listen(5500)
+
+function broadcast(str) {
+    server.connections.forEach(function (connection) {
+        connection.sendText(str)
+    })
+}
